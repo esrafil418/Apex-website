488336b5953dc13f01e9fb6643f43ef12a59336a
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-18 21:44:40 +0330

    feat: Add LandingCounter component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,7 @@ import "./Header.css";
 
 export default function Header() {
 	return (
-		<div>
+		<div className="header">
 			<Topbar />
 			<Navbar />
 			<Landing />
---
+++ src/components/landingCounter/LandingCounter.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+type LandingCounterProps = {
+	count: number;
+};
+
+export default function LandingCounter({ count }: LandingCounterProps) {
+	const [courseCounter, setCourseCounter] = useState(0);
+
+	useEffect(() => {
+		let interval = setInterval(() => {
+			setCourseCounter((prevCount) => prevCount + 1);
+		}, 1);
+
+		if (courseCounter === count) {
+			clearInterval(interval);
+		}
+
+		return () => clearInterval(interval);
+	}, [courseCounter]);
+
+	return <span className="landing-status__count">{courseCounter}</span>;
+}
