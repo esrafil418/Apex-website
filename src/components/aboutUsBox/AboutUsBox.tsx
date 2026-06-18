34972adfd2ffc06e84006133c8af2bc1418041bf
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-18 21:33:08 +0330

    feat: Add styles and props to AboutUs component

--- src/components/aboutUsBox/AboutUsBox.tsx
+++ src/components/aboutUsBox/AboutUsBox.tsx
@@ -1,5 +1,23 @@
 import "./AboutUsBox.css";
 
-export default function AboutUsBox() {
-	return <div>AboutUsBox</div>;
+type AboutUsBoxProps = {
+	title: string;
+	desc: string;
+	icon: string;
+};
+
+export default function AboutUsBox({ title, desc, icon }: AboutUsBoxProps) {
+	return (
+		<div className="col-6">
+			<div className="about-us__box">
+				<div className="about-us__box-right">
+					<i className="far fa-copyright about-us__icon"></i>
+				</div>
+				<div className="about-us__box-left">
+					<span className="about-us__box-title">{title}</span>
+					<span className="about-us__box-text">{desc}</span>
+				</div>
+			</div>
+		</div>
+	);
 }
